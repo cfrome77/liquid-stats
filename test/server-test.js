e24abb0ce847c802d7851ccaa6d5dd01397b0b0e
cfrome77 <fromeca@dukes.jmu.edu>
2019-10-16 22:16:32 -0400

    Update to test that the app laods correctly

--- test/server-test.js
+++ test/server-test.js
@@ -1,13 +1,22 @@
-var expect  = require('chai').expect;
-var request = require('request');
+let request = require('request');
+let chai = require('chai');
+let assert = require('chai').assert;
+let expect = require('chai').expect;
+let server = require('../app');
+let should = chai.should();
+let chaiHttp = require('chai-http');
 
-describe('Status', function() {
-    describe ('Main page', function() {
-        it('status', function(done){
-            request('http://localhost:8080/', function(error, response, body) {
-                expect(response.statusCode).to.equal(200);
-                done();
-            });
-        });
-    });
+chai.use(chaiHttp);
+
+describe("/GET  home page",() => {
+it('should return status 200 ', (done) => {
+chai.request(server)
+.get('/') 
+.end((err, res) => {
+  should.not.exist(err);
+  should.exist(res);
+  res.should.have.status(200);
+  done();
+  });
+ });
 });
