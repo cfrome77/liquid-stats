832a26fbf0b97e30a935633eb6f1f4e113844168
cfrome77 <fromeca@dukes.jmu.edu>
2020-12-17 21:25:23 -0500

    remove files not needed for angular

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const dotenv = require('dotenv');
-dotenv.config();
-module.exports = {
-  CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-  UNTAPPD_USERNAME: process.env.UNTAPPD_USERNAME
-};
