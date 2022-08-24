42ec283e4a069369218a4c9e25e96275ebbcfc97
cfrome77 <cfrome777@gmail.com>
2022-08-24 13:31:43 -0400

    Update to fix heroku error

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/liquid-stats'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/liquid-stats/'}),
-);
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
