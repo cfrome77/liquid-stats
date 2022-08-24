de31af5798c0073556e115d22793b5b7039de5ae
cfrome77 <cfrome777@gmail.com>
2022-08-24 13:44:12 -0400

    Fix server to use index.html from production dist

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/liquid-stats'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/liquid-stats/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
