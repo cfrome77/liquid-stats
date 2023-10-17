e899ceb011935348b2321934bce1f37732eca878
cfrome77 <cfrome777@gmail.com>
2023-10-17 16:26:51 -0400

    Update to remove console logs

--- src/app/_services/pop-up.service.ts
+++ src/app/_services/pop-up.service.ts
@@ -8,9 +8,6 @@ export class PopUpService {
   constructor() { }
 
   makePopup(brewery_name: any, brewery_state: any, brewery_city: any): string {
-    console.log(brewery_name);
-    console.log(brewery_city);
-    console.log(brewery_state);
     return `` +
       `<div>${ brewery_name }</div>` +
       `<div>${ brewery_city } ${ brewery_state}</div>`;
