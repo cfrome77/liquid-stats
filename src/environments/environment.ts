ca83baa9721d8e28457157307886e9e4996a0384
cfrome77 <cfrome777@gmail.com>
2026-02-27 16:38:20 -0500

    Fix env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: 'false',
-  DATA_URL: 'https://liquid-stats-46ea8b54.s3.amazonaws.com/',
-  UNTAPPD_USERNAME: 'fromeca',
-  GA4_MEASUREMENT_ID: 'G-K425XQJT21'
+  production: '__PRODUCTION__',
+  DATA_URL: '__DATA_URL__',
+  UNTAPPD_USERNAME: '__UNTAPPD_USERNAME__',
+  GA4_MEASUREMENT_ID: '__GA4_MEASUREMENT_ID__'
 };
