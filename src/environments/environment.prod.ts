fcded03053154da6ea96e9bf5c2468c6bfee1e1b
cfrome77 <cfrome777@gmail.com>
2026-03-26 01:06:45 -0400

    chore: fix gitignore and stop tracking environment files

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: '__PRODUCTION__',
-  DATA_URL: '__DATA_URL__',
-  UNTAPPD_USERNAME: '__UNTAPPD_USERNAME__',
-  GA4_MEASUREMENT_ID: '__GA4_MEASUREMENT_ID__'
-};
